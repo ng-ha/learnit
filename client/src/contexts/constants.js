6d290f259aec59259c16536844bca2780e9afa3e
ng-ha <ngthanhha.hn@gmail.com>
2023-05-11 15:53:05 +0700

    setup deployment

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
-  process.env.NODE_ENV !== 'production'
+  process.env.node_env !== 'production'
     ? 'http://localhost:5000/api'
-    : 'https://delightful-pantsuit-hare.cyclic.app/api';
+    : 'https://learnit-0hto.onrender.com/api';
 // : 'https://motionless-moose.cyclic.app/api';
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
